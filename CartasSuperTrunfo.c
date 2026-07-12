f7bf8ce450cb7c3e6c210c8b1ce694f4d1ccc430
BetoDolphin <lucasrobertoramospereira1@gmail.com>
2026-07-12 14:14:37 +0000

    introduzi os dados/comandos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,9 +6,27 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  
+  char nome[50];
+  int populacao;
+  float area;
+  float pib;
   // Área para entrada de dados
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome);
 
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+
+    printf("Nome: %s\n", nome);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %f\n", pib);
   // Área para exibição dos dados da cidade
 
 return 0;
